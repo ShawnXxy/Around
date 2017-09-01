199ae5014f52b78c86f51c31969dae96f32414c4
ShawnXxy <shawnxxy@hotmail.com>
2017-08-31 22:50:48 -0500

    fragment created

--- app/src/main/java/site/shawnxxy/eventreporter/MainActivity.java
+++ app/src/main/java/site/shawnxxy/eventreporter/MainActivity.java
@@ -1,35 +1,59 @@
 package site.shawnxxy.eventreporter;
 
-import android.support.v7.app.AppCompatActivity;
+//import android.app.Fragment;
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements EventFragment.OnItemSelectListener {
+    private EventFragment mListFragment;
+    private CommentFragment mGridFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("Life cycle test", "We are at onCreate()");
+        Log.e("Life cycle test", "We are at onCreate()"); // TEST in log
 
+        // Show different fragments based on screen size.
+//        if (findViewById(R.id.fragment_container) != null) {
+//            Fragment fragment = isTablet() ? new CommentFragment() : new EventFragment();
+//            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, fragment).commit();
+//        }
+
+        //add list view
+        mListFragment = new EventFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.event_container, mListFragment).commit();
+
+        //add Gridview
+        if (isTablet()) {
+            mGridFragment = new CommentFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.comment_container, mGridFragment).commit();
+        }
+
+        /**
+         *  Below section is not needed for Fragment
+         */
         // Get ListView object from xml.
-        ListView eventListView = (ListView) findViewById(R.id.event_list);
-
-        // Initialize an adapter.
+//        ListView eventListView = (ListView) findViewById(R.id.event_list);
+//
+//        EventAdapter adapter = new EventAdapter(this);
+//
+//        // Assign adapter to ListView.
+//        eventListView.setAdapter(adapter);
+//
+//        // Initialize an adapter. Used for the dummy function test below
 //        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
 //                this,
 //                R.layout.event_item,
 //                R.id.event_name,
 //                getEventNames()
 //        );
-
-        EventAdapter adapter = new EventAdapter(this);
-
-        // Assign adapter to ListView.
-        eventListView.setAdapter(adapter);
-    }
+    } // END OF onCreate()
 
     /**
      * A dummy function to get fake event names.
@@ -44,6 +68,12 @@ public class MainActivity extends AppCompatActivity {
 //                "Event10", "Event11", "Event12"};
 //        return names;
 //    }
+
+    private boolean isTablet() {
+        return (getApplicationContext().getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK) >=
+                Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
 
     @Override
     protected void onStart() {
@@ -75,4 +105,8 @@ public class MainActivity extends AppCompatActivity {
         Log.e("Life cycle test", "We are at onDestroy()");
     }
 
+    @Override
+    public void onItemSelected(int position) { // defined in CommentFragment
+        mGridFragment.onItemSelected(position);
+    }
 }
