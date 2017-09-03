02dd4fc424507275782f3dfcfc28597b8cf06cbe
ShawnXxy <shawnxxy@hotmail.com>
2017-09-03 17:36:07 -0500

    user ui with registration and login

--- app/src/main/java/site/shawnxxy/eventreporter/MainActivity.java
+++ app/src/main/java/site/shawnxxy/eventreporter/MainActivity.java
@@ -6,15 +6,28 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity implements EventFragment.OnItemSelectListener, CommentFragment.OnItemSelectListener {
     private EventFragment mListFragment;
     private CommentFragment mGridFragment;
+
+    private EditText mUsernameEditText;
+    private EditText mPasswordEditText;
+    private Button mSubmitButton;
+    private Button mRegisterButton;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +36,76 @@ public class MainActivity extends AppCompatActivity implements EventFragment.OnI
         Log.e("Life cycle test", "We are at onCreate()"); // TEST in log
 
         // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        myRef.setValue("Hello, World!");
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
+//        myRef.setValue("Hello, World!");
+
+        /**
+         *  Initialize widgets in the activity
+         */
+        // Firebase uses singleton to initialize the sdk
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mUsernameEditText = (EditText) findViewById(R.id.editTextLogin);
+        mPasswordEditText = (EditText) findViewById(R.id.editTextPassword);
+        mSubmitButton = (Button) findViewById(R.id.submit);
+        mRegisterButton = (Button) findViewById(R.id.register);
+
+        /**
+         *  Implements button registration click event
+         */
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = mUsernameEditText.getText().toString();
+                final String password = Utils.md5Encryption(mPasswordEditText.getText().toString());
+                final User user = new User(username, password, System.currentTimeMillis());
+                mDatabase.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChild(username)) {
+                            Toast.makeText(getBaseContext(),"username is already registered, please change one", Toast.LENGTH_SHORT).show();
+                        } else if (!username.equals("") && !password.equals("")){
+                            // put username as key to set value
+                            mDatabase.child("users").child(user.getUsername()).setValue(user);
+                            Toast.makeText(getBaseContext(),"Successfully registered", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+        /**
+         *  Implements login click event
+         */
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = mUsernameEditText.getText().toString();
+                final String password = Utils.md5Encryption(mPasswordEditText.getText().toString());
+                mDatabase.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChild(username) && (password.equals(dataSnapshot.child(username).child("password").getValue()))) {
+                            Log.i( " Your log", "You successfully login");
+                        } else {
+                            Toast.makeText(getBaseContext(),"Please login again", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
 
         // Show different fragments based on screen size.
 //        if (findViewById(R.id.fragment_container) != null) {
