ea8291d5802b62e9181839956d19ff2e47027024
ShawnXxy <shawnxxy@hotmail.com>
2017-10-09 16:07:50 -0500

    profile adapter

--- app/src/main/java/site/shawnxxy/eventreporter/utils/Utils.java
+++ app/src/main/java/site/shawnxxy/eventreporter/utils/Utils.java
@@ -1,8 +1,14 @@
 package site.shawnxxy.eventreporter.utils;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.os.Build;
 import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
 
 import org.apache.commons.codec.binary.Hex;
 
@@ -81,5 +87,43 @@ public class Utils {
             ex.printStackTrace();
         }
         return result;
+    }
+
+    /**
+     *  Get screen size
+     */
+    private static int screenWidth = 0;
+    private static int screenHeight = 0;
+
+    public static int dpToPx(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+
+    public static int getScreenHeight(Context c) {
+        if (screenHeight == 0) {
+            WindowManager wm = (WindowManager) c.getSystemService(Context.WINDOW_SERVICE);
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            screenHeight = size.y;
+        }
+
+        return screenHeight;
+    }
+
+    public static int getScreenWidth(Context c) {
+        if (screenWidth == 0) {
+            WindowManager wm = (WindowManager) c.getSystemService(Context.WINDOW_SERVICE);
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            screenWidth = size.x;
+        }
+
+        return screenWidth;
+    }
+
+    public static boolean isAndroid5() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
     }
 }
